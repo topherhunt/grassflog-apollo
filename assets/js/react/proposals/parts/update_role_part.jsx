123346c0d8da1bf6ee23e9e2c3dffb80730b7d0d
Topher Hunt <hunt.topher@gmail.com>
2019-05-11 16:26:03 +0200

    (WIP) ProposalBuilder: Write formObject<->changeList conversion functions

--- assets/js/react/proposals/parts/update_role_part.jsx
+++ assets/js/react/proposals/parts/update_role_part.jsx
@@ -49,84 +49,189 @@ const ConvertHolarchyToList = {
 }
 
 const Simulator = {
-  // Given a starting state and a list of changes, execute each change and return the
-  // resulting state.
-  runChanges: function(currentState, changes) {
-    startingState = clone(currentState) // make sure we don't mutate currentState itself
-    return _.reduce(changes, (memo, i) => this.simulateChange(memo, i), startingState)
+  sampleStartingFormObject: {
+    type: "update_role",
+    targetId: 27,
+    attrs: {
+      name: "the orig name",
+      purpose: "the orig purpose"
+    },
+    expandRole: false,
+    collapseRole: false,
+    deleteRole: false,
+    moveRoles: [],
+    domains: [
+      {id: 1, name: "an existing domain"}
+    ],
+    accts: [
+      {id: 1, name: "an existing acct"}
+    ]
   },
 
-  // Execute a single change and return the transformed state.
+  // This sample shows the expected form object shape for an UpdateRole proposal part.
+  // It should closely mirror the UI and form elements, to make the event listeners as
+  // simple as possible.
+  // Some of the form's contents look like change instructions but others look more like
+  // representations of db records.
+  sampleUpdatedFormObject: {
+    type: "update_role",
+    targetId: 27,
+    attrs: {
+      name: "blah",
+      purpose: "blah"
+    },
+    expandRole: true,
+    collapseRole: false,
+    deleteRole: false,
+    moveRoles: [{id: 3, parentId: 5}],
+    domains: [
+      {id: 1, name: "a domain"},
+      {id: 2, name: "blah updated", update: true},
+      {id: null, name: "a new domain", create: true},
+      {id: 4, name: "blah deleted", delete: true},
+    ],
+    accts: [
+      {id: 1, name: "an acct"},
+      {id: 2, name: "blah updated", update: true},
+      {id: null, name: "a new acct", create: true},
+      {id: 4, name: "blah deleted", delete: true},
+    ]
+  },
+
+  // Given a starting Form object and a list of changes, execute each change and return
+  // the resulting form object.
+  simulateChanges: function(form, changes) {
+    startingForm = clone(currentState) // make sure we don't mutate currentState itself
+    return _.reduce(changes, (memo, i) => this.simulateChange(memo, i), startingForm)
+  },
+
+  // Execute a single change and return the transformed Form object.
   // This is basically a reducer.
-  // Each of these functions should look very similar to my UI handlers, or
-  // maybe even can be the same as them.
-  simulateChange: function(state, change) {
+  simulateChange: function(form, change) {
     type = change.type
-    if (type == "create_role") {
-      TODO
-    } else if (type == "blah") {
-
-    } else {
-      raise("Unknown type: "+type)
+    if (type == "update_role") {
+      if (change.params.name) { form.attrs.name = change.params.name }
+      if (change.params.purpose) { form.attrs.purpose = change.params.purpose }
     }
+    else if (type == "expand_role") {
+      form.expandRole = true
+    }
+    else if (type == "collapse_role") {
+      form.collapseRole = true
+    }
+    else if (type == "delete_role") {
+      form.deleteRole = true
+    }
+    else if (type == "move_role") {
+      if (!form.moveRoles) { form.moveRoles = [] }
+      form.moveRoles.concat({id: change.targetId, parentId: change.params.parent_id})
+    }
+    else if (type == "create_domain") {
+      form.domains.concat({
+        roleId: change.params.role_id,
+        name: change.params.name,
+        create: true
+      })
+    }
+    else if (type == "update_domain") {
+      domain = form.domains.find((d) d.id == change.targetId)
+      domain.name = change.params.name
+      domain.update = true
+    }
+    else if (type == "delete_domain") {
+      domain = form.domains.find((d) d.id == change.targetId)
+      domain.delete = true
+    }
+    else if (type == "create_acct") {
+      form.accts.concat({
+        roleId: change.params.role_id,
+        name: change.params.name,
+        create: true
+      })
+    }
+    else if (type == "update_acct") {
+      acct = form.accts.find((d) d.id == change.targetId)
+      acct.name = change.params.name
+      acct.update = true
+    }
+    else if (type == "delete_acct") {
+      acct = form.accts.find((d) d.id == change.targetId)
+      acct.delete = true
+    }
+    else {
+      raise("Unknown ProposalChange type: "+type)
+    }
+
+    return form
   },
 
-  // Given an old state and a new (or desired) state, compute what changes will bring you
-  // from the old state to the new state.
-  computeChanges: function(prevState, nextState) {
+  // Given two form states (orig and next), compute what changes are required to transform
+  // orig into next. Return the list of changes.
+  computeChanges: function(origForm, nextForm) {
     let changes = []
-    const nextRoles = nextState.filter((i) => (i.type == "role"))
-    const nextDomains = nextState.filter((i) => (i.type == "domain"))
-    const nextAccts = nextState.filter((i) => (i.type == "acct"))
+    let roleId = nextForm.targetId
 
-    // Any newly created roles?
-    nextRoles.filter(function(i) {
-      prev = this.findRecord(prevState, "role", i.id)
-      if (!prev) {
-        changes.concat({
-          type: "create_role",
-          targetId: null,
-          params: this.cleanParams(role, ["parent_id:req", "name:req", "purpose:opt"])
-        })
-      }
+    // Is the role updated?
+    if (origForm.name != nextForm.name || origForm.purpose != nextForm.purpose) {
+      let params = {}
+      if (origForm.name != nextForm.name) { params.name = nextForm.name }
+      if (origForm.purpose != nextForm.purpose) { params.purpose = nextForm.purpose }
+      changes.concat({type: "update_role", targetId: roleId, params: params})
     }
 
-    // Any updated roles?
-    nextRoles.filter(function(role) {
-      prev = this.findRecord(prevState, "role", role.id)
-      if (prev && (prev.name != role.name || prev.purpose != role.purpose)) {
-        let params = this.cleanParams(role, ["name:opt", "purpose:opt"])
-        if (role.name == prev.name) { delete params.name }
-        if (role.purpose == prev.purpose) { delete params.purpose }
-        changes.concat({
-          type: "update_role",
-          targetId: role.id,
-          params: params
-        })
-      }
+    // Is the role being expanded?
+    if (nextForm.expandRole) {
+      changes.concat({type: "expand_role", targetId: roleId})
+    }
+
+    // Are roles being moved in or out?
+    nextForm.moveRoles.map(function(move) {
+      changes.concat({targetId: move.targetId, parentId: move.parentId})
     })
 
-    // Any new domains?
+    // Is the role being collapsed?
+    if (nextForm.collapseRole) {
+      changes.concat({type: "collapse_role", targetId: roleId})
+    }
 
+    // Is the role being deleted?
+    if (nextForm.deleteRole) {
+      changes.concat({type: "delete_role", targetId: roleId})
+    }
 
-    // ...
+    // Are new domains added?
+    nextForm.domains.filter((i) => !!i.create).map(function(domain) {
+      changes.concat({type: "create_domain", params: {roleId: roleId, name: domain.name}})
+    })
+
+    // Are existing domains updated?
+    nextForm.domains.filter((i) => !!i.update).map(function(domain) {
+      changes.concat({type: "update_domain", targetId: domain.id, name: domain.name})
+    })
+
+    // Are domains removed?
+    nextForm.domains.filter((i) => !!i.delete).map(function(domain) {
+      changes.concat({type: "delete_domain", targetId: domain.id})
+    })
+
+    // Are new accts added?
+    nextForm.accts.filter((i) => !!i.create).map(function(acct) {
+      changes.concat({type: "create_acct", params: {roleId: roleId, name: acct.name}})
+    })
+
+    // Are existing accts updated?
+    nextForm.accts.filter((i) => !!i.update).map(function(acct) {
+      changes.concat({type: "update_acct", targetId: acct.id, name: acct.name})
+    })
+
+    // Are accts removed?
+    nextForm.accts.filter((i) => !!i.delete).map(function(acct) {
+      changes.concat({type: "delete_acct", targetId: acct.id})
+    })
+
+    console.log("TODO: Run changes on origForm to confirm that we end up identical to nextForm.")
+
     return changes
-  },
-
-  findRecord: function(state, type, id) {
-    return state.find((i) => (i.type == type && i.id == id))
-  },
-
-  //
-  cleanParams: function(params, rules) {
-    let cleaned = {}
-    rules.map(function(rule){
-      [field, setting] = rule.split(":")
-      if (!["req", "opt"].includes(setting)) { raise("Unknown setting: "+setting) }
-      if (setting == "req" && !params[field]) { raise("Required field is blank: "+field) }
-      cleaned[field] = params[field]
-    })
-    return cleaned
   }
 }
 
